2f51f210679cbe4f4c5b6909aed53ab6e4532228
Alexandroni07 <gaalexandroni@gmail.com>
2025-06-02 18:03:24 -0300

    binarySearch

--- arrays/twoPointer.py
+++ arrays/twoPointer.py
@@ -1,20 +1,19 @@
 class Solution:
     def reverseWords(self, s):
-        res = ''
-        l, r = 0, 0
-
+        res = ''  # String que armazenará o resultado final
+        l, r = 0, 0  # Ponteiros esquerdo (l) e direito (r) para marcar o início e fim de cada palavra
+        
         while r < len(s):
             if s[r] != ' ':
+                # Se o caractere atual não é espaço, movemos o ponteiro direito para frente
                 r += 1
             else:
-                res += s[l:r+1][::-1]
-                r += 1
-                l = r
-
+                # Quando encontramos um espaço, pegamos a palavra (de l até r) e a invertemos
+                res += s[l:r+1][::-1]  # Adiciona a palavra invertida + espaço ao resultado
+                r += 1  # Move r para o próximo caractere (início da próxima palavra)
+                l = r   # Atualiza l para a posição de r (início da nova palavra)
+        
+        # Após o loop, adiciona a última palavra (que não foi processada no loop)
         res += ' '
         res += s[l:r + 2][::-1]
-        return res[1:]
-    
-s = "Let's take LeetCode contest"
-solution = Solution()
-print(solution.reverseWords(s))  # Output: "s'teL ekat edoCteeL tsetnoc"
+        return res[1:]  # Retorna o resultado removendo o espaço extra no início
